3a58d1c802568db880008be686cc527c11297f0b
salman-frs <sallman.alffarisi@gmail.com>
2025-07-31 02:28:15 +0700

    Implement code changes to enhance functionality and improve performance

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? undefined : '',
 }
 
 module.exports = nextConfig
